066e95784947d2cfb73a68ebb522de91b2c375ca
coltongraygg <163790834+coltongraygg@users.noreply.github.com>
2024-05-27 17:46:25 +0000

    Added Studies folder with core concept study files

---
+++ Studies/variables.js
@@ -0,0 +1,42 @@
+/** 
+ * VARIABLES:
+ * 
+ * 0. To hold things in memory during the life cycle of a 
+ * program, we can use variables. Variables are named
+ * identifiers that can point to values of a particular
+ * type, like a Number, String, Boolean, Array, Object
+ * or another data-type. Variables are called so because
+ * once created, we can CHANGE the value (and type of value)
+ * to which they point.
+ * 
+ * 1. To create a variable we use the keyword, var, 
+ * followed by a name (id or alias) for our variable.
+ * 
+ * 2. There are 2 phases of using variables: declaration
+ * and initialization (or assignment).
+ */
+
+// 1. Declaration 
+// At the declaration phase, the variable myName is undefined because we have NOT initialized it to anything
+var myName;
+console.log(myName); // prints => undefined
+
+// 2. Initialization or Assignment
+// Hwne you assign a value to an undeclared variable
+myName = 'john';
+console.log(myName); // prints => john
+
+// 3. Re-Assignment 
+// Changing or updating the value of a previously assigned variable
+myName = 'bob';
+console.log(myName); // prints => bob
+
+// 4. Var, let, and const
+
+// var example //
+
+
+// let example //
+
+
+// const example //
